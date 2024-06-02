9cd988dad045a15023c459b64d1beacb04ea97da
rainbowpython <joshble@outlook.com>
2024-06-02 00:02:35 -0700

    gray

--- src/client/java/com/example/TemplateModClient.java
+++ src/client/java/com/example/TemplateModClient.java
@@ -1,29 +1,12 @@
-package com.example.name.modid;
+package com.example;
 
-import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
-import net.minecraft.client.render.RenderLayer;
+import net.fabricmc.api.ClientModInitializer;
 
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import com.example.name.modid.block.ModBlocks;
-
-public class LanternMod implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-    public static final Logger LOGGER = LoggerFactory.getLogger("lantern-mod");
-	public static final String MOD_ID = "lantern-mod";
+public class TemplateModClient implements ClientModInitializer {
 	@Override
-	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-		ModBlocks.registerModBlocks();
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
 		
-
 		LOGGER.info("Hello Fabric world!");
 		BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.RED_LANTERN, RenderLayer.getCutout());
 		BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.YELLOW_LANTERN, RenderLayer.getCutout());
